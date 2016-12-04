f4400c74992fed27ab77bd4d13bee37a4faa7b92
mdoto <mdoto@mdoto-Elitebook>
2016-12-04 14:50:51 -0500

    New Structure

---
+++ GUIProj/src/game/Trivia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package game;
+
+/**
+ * I pledge my honor that I have abided by the Stevens Honor System
+ * -- Matthew Doto
+ *
+ * @author mdoto
+ * @version 1.0.0
+ * @since Dec 4, 2016
+ */
+public class Trivia {
+
+}
